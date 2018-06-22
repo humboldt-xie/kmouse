b6f1af06e6a7ba041040a27ad242f01fb687e5c2
humboldt <humboldt_xie@163.com>
2018-06-22 11:51:57 +0800

    add keylayout for mac

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 
 一种使用键盘来控制鼠标动作的demo 与尝试
+
+
+## set layout
+
+http://wordherd.com/keyboards/
+Download the key mapping file My Layout.keylayout.
+Move it to ~/Library/Keyboard\ Layouts/
+Open System Preferences -> Language & Text
+Go to Input Sources -> (hit +) -> Select Others
+You should find My Layout in the list and select it.
+
+## dep
+
+```
+pip3 install -U pyobjc
+pip3 install pyobjc-core
+pip3 install pyautogui
+pip3 install keyboard
+```
+
--- kmouse.py
+++ kmouse.py
@@ -1,29 +1,61 @@
+#!env python3
 import pyautogui
+import fcntl
 
 import keyboard
-i=0
+switch=False
+step=1
+pyautogui.FAILSAFE=False
+
+
+def on_down(e):
+    global switch
+    if e.scan_code==58 or e.scan_code==61:
+        switch=True
+
+def on_up(e):
+    global switch
+    if e.scan_code==58 or e.scan_code==61:
+        switch=False
 
 def on_press(e):
-    global i
+    global switch,step
+    print("press:",e.event_type,e.scan_code," ",e.name,switch,step)
+    if e.event_type=='up':
+        on_up(e)
+    if e.event_type=='down':
+        on_down(e)
+        return
     x,y=pyautogui.position()
-    if e.name=='a':
-        pyautogui.moveTo(x-10, y)
-    if e.name=='d':
-        pyautogui.moveTo(x+10, y)
-    if e.name=='s':
-        pyautogui.moveTo(x, y+10)
-    if e.name=='w':
-        pyautogui.moveTo(x, y-10)
-    if e.event_type=='up' and e.scan_code==110:
-        i=(i+1)%2
-    #print("press:",e.event_type,e.scan_code," ",e.name,i)
-def on_release(e):
-    if e.scan_code==110:
-        i=(i+1)%2
-    print("status",i)
+    if not switch:
+        return True
+    if e.name>='1' and e.name<='9':
+        s=ord(e.name)-ord('1')+1
+        step=1<<s
+    if e.name=='h':
+        pyautogui.moveTo(x-step, y)
+    if e.name=='l':
+        pyautogui.moveTo(x+step, y)
+    if e.name=='j':
+        pyautogui.moveTo(x, y+step)
+    if e.name=='k':
+        pyautogui.moveTo(x, y-step)
+    if e.name=='n':
+        pyautogui.click(button='left')
+    if e.name=='m':
+        pyautogui.click(button='right')
+    return False
     #pyautogui.moveTo(200, 150)
     #print("release:",e.scan_code)
-keyboard.hook(on_press)
 #keyboard.on_release(on_release)
 
-keyboard.wait()
+try:  
+    fp = open('/tmp/kmouse','w')  
+    fcntl.flock(fp.fileno(), fcntl.LOCK_EX | fcntl.LOCK_NB)  
+    keyboard.hook(on_press)
+    print('start')
+    keyboard.wait()
+    fp.close()  
+except IOError:  
+    print("lock")
+
